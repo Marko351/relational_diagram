b4269e52419bda2a77bad1f7897aa3165d665291
Marko351 <markodjuric351@gmail.com>
2023-06-08 09:04:18 +0200

    setting up tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      screens: {
+        tablet: '640px',
+        laptop: '1024px',
+        desktop: '1280px',
+      },
+    },
+  },
+  plugins: [],
+}
